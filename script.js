c5423d81414234ad09d9c82dcb690d3620f75923
Jack E. Shomer <74559111+jsho982045@users.noreply.github.com>
2024-06-02 22:33:51 +0000

    Initial Commit - using Deck of Cards api for the cards, need to add dealer, chips, and table as well as fix some of the rules

---
+++ script.js
@@ -0,0 +1,152 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const dealButton = document.getElementById("deal-button");
+    const hitButton = document.getElementById("hit-button");
+    const standButton = document.getElementById("stand-button");
+    const dealerCardsElement = document.getElementById("dealer-cards");
+    const playerCardsElement = document.getElementById("player-cards");
+    const dealerScoreElement = document.getElementById("dealer-score");
+    const playerScoreElement = document.getElementById("player-score");
+    const messageElement = document.getElementById("message");
+
+    let dealerCards = [];
+    let playerCards = [];
+    let deckId;
+    let dealerScore = 0;
+    let playerScore = 0;
+
+    function createDeck() {
+        return fetch('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1')
+            .then(response => response.json())
+            .then(data => {
+                deckId = data.deck_id;
+                dealerCards = [];
+                playerCards = [];
+                return deckId;
+            });
+    }
+
+    function drawCards(count) {
+        return fetch(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=${count}`)
+            .then(response => response.json())
+            .then(data => data.cards);
+    }
+
+    function getCardValue(card) {
+        if (card.value === "ACE") {
+            return 11;
+        } else if (["KING", "QUEEN", "JACK"].includes(card.value)) {
+            return 10;
+        } else {
+            return parseInt(card.value);
+        }
+    }
+
+    function updateScores() {
+        dealerScore = calculateScore(dealerCards);
+        playerScore = calculateScore(playerCards);
+        dealerScoreElement.textContent = `Score: ${dealerScore}`;
+        playerScoreElement.textContent = `Score: ${playerScore}`;
+    }
+
+    function calculateScore(cards) {
+        let score = 0;
+        let hasAce = false;
+        for (let card of cards) {
+            score += getCardValue(card);
+            if (card.value === "ACE") {
+                hasAce = true;
+            }
+        }
+        if (hasAce && score > 21) {
+            score -= 10;
+        }
+        return score;
+    }
+
+    function renderCards() {
+        dealerCardsElement.innerHTML = dealerCards.map(card => getCardHTML(card)).join("");
+        playerCardsElement.innerHTML = playerCards.map(card => getCardHTML(card)).join("");
+    }
+
+    function getCardHTML(card) {
+        return `<div class="card" style="background-image: url('${card.image}');"></div>`;
+    }
+
+    function checkForEndGame() {
+        if (playerScore === 21) {
+            messageElement.textContent = "Blackjack! You win!";
+            endGame();
+        } else if (playerScore > 21) {
+            messageElement.textContent = "You busted! Dealer wins!";
+            endGame();
+        } else if (dealerScore === 21) {
+            messageElement.textContent = "Dealer has Blackjack! Dealer wins!";
+            endGame();
+        } else if (dealerScore > 21) {
+            messageElement.textContent = "Dealer busted! You win!";
+            endGame();
+        }
+    }
+
+    function endGame() {
+        hitButton.disabled = true;
+        standButton.disabled = true;
+        dealButton.disabled = false;
+    }
+
+    dealButton.addEventListener("click", function() {
+        createDeck().then(() => {
+            drawCards(4).then(cards => {
+                dealerCards = [cards[0], cards[1]];
+                playerCards = [cards[2], cards[3]];
+                updateScores();
+                renderCards();
+                messageElement.textContent = "";
+                hitButton.disabled = false;
+                standButton.disabled = false;
+                dealButton.disabled = true;
+                checkForEndGame();
+            });
+        });
+    });
+
+    hitButton.addEventListener("click", function() {
+        drawCards(1).then(cards => {
+            playerCards.push(cards[0]);
+            updateScores();
+            renderCards();
+            checkForEndGame();
+        });
+    });
+
+    standButton.addEventListener("click", function() {
+        function dealerPlay() {
+            if (dealerScore < 17) {
+                drawCards(1).then(cards => {
+                    dealerCards.push(cards[0]);
+                    updateScores();
+                    renderCards();
+                    if (dealerScore < 17) {
+                        dealerPlay();
+                    } else {
+                        determineWinner();
+                    }
+                });
+            } else {
+                determineWinner();
+            }
+        }
+        dealerPlay();
+    });
+
+    function determineWinner() {
+        if (dealerScore > playerScore && dealerScore <= 21) {
+            messageElement.textContent = "Dealer wins!";
+        } else if (dealerScore === playerScore) {
+            messageElement.textContent = "It's a tie!";
+        } else {
+            messageElement.textContent = "You win!";
+        }
+        endGame();
+    }
+});
